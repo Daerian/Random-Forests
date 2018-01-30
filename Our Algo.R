f12541b2e166645325e30e2e0b6dbd357e669853
Daerian Dilkumar <daerian.dilkumar@hotmail.com>
2018-01-30 14:20:08 -0500

    Rearranged and fixed bug fix
    Better sorted segments,
    worked on the building part

--- Our Algo.R
+++ Our Algo.R
@@ -13,13 +13,12 @@ require(tidyverse)
 require(rpart)
 require(rpart.plot)
 
-=======
-
 library(tidyverse)
 library(rpart)
 library(rpart.plot)
 
-=======
+
+################################### READ AND CLEAN DATA ############################################
 
 redWineData = read_delim("winequality-red.csv", delim = ";")
 whiteWineData = read_delim("winequality-white.csv", delim = ";")
@@ -33,18 +32,17 @@ whiteWineData = whiteWineData %>% mutate (Type = "White")
 
 # Merge Data and get Final Dataset
 wineData = rbind(redWineData,whiteWineData)
-
-# Glimpse of the Data Set
-glimpse(wineData)
-
+# Remove spaces from column names
 names(wineData) = gsub(" ","_", names(wineData))
+
+
+
+##################################### BUILD A TREE ##############################################
 
 sample.wineData = sample_n(wineData, 1000, replace=TRUE)
 sample.p.wineData = sample(sample.wineData[,c(-12,-13)], 4, replace=FALSE)
+param = paste(names(sample.p.wineData)[5],"~", paste(names(sample.p.wineData)[-1], collapse=" + "))
 sample.p.wineData$quality = sample.wineData$quality
 column.names = colnames(sample.p.wineData)
-param = paste(names(sample.p.wineData)[5],"~", paste(names(sample.p.wineData)[-1], collapse=" + "))
 trees=rpart(formula=param, data=sample.p.wineData, method='class')
-plot(trees)
-text(trees, use.n=TRUE)
-
+rpart.plot(trees)
