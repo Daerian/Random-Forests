69bf97d5e1189ae9a894c432dc10b6495babe846
Kevin Chen <guanyukevin.chen@gmail.com>
2018-03-20 15:38:09 -0400

    WORKING RMD!!!!!! xD

--- randomforest.R
+++ randomforest.R
@@ -102,7 +102,7 @@ Classify = function(forest, obs){
   i = 0
   # This for loop will add the predictions of every tree together, so they can be aggregated
   for (i in 1:numTrees){
-    predictions = predict(forest[[i]][[1]], obs, type = "class")
+    predictions = predict(forest[[i]][[1]], obs)
   }
   return (predictions)
 }
